8b9ab128497130629f73a0417648a5ed2107a295
YILIN <YILIN@DESKTOP-YILIN>
2022-05-26 15:32:28 +0800

    init

---
+++ nsis/version.nsh
@@ -0,0 +1,5 @@
+!define VERSION "2.16rc0"
+!define MAJOR_VER 15
+!define MINOR_VER 15
+!define SUBMINOR_VER 99
+!define PATCHLEVEL_VER 90
---
+++ version.h
@@ -0,0 +1,9 @@
+#ifndef NASM_VERSION_H
+#define NASM_VERSION_H
+#define NASM_MAJOR_VER      2
+#define NASM_MINOR_VER      15
+#define NASM_SUBMINOR_VER   99
+#define NASM_PATCHLEVEL_VER 90
+#define NASM_VERSION_ID     0x020f635a
+#define NASM_VER            "2.16rc0"
+#endif /* NASM_VERSION_H */
